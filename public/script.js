5431c489cbeba9715569efde278d66f02768069b
Caren Garcia <carenvaleria90@gmail.com>
2024-07-15 15:01:38 -0600

    updating directory

---
+++ public/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('alert-button').addEventListener("click", function() {
+        alert('Hello, world!');
+    });
+});
+
+
+//alternative
+
+function alertMessage(){
+    alert('Hello World!');
+}
